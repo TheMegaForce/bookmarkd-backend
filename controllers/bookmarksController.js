477de44aae36d59d57e69289544d95d0c0cfdde3
Ishan Azad <ishansazad@gmail.com>
2023-09-19 17:53:17 -0700

    adding all of backend

---
+++ controllers/bookmarksController.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const Bookmark = require("../models/Bookmarks.js");
+const router = express.Router()
+
+// INDUCES
+// Show Index create delete update
+
+// Index
+router.get("/", async (req, res) => {
+    try {
+        res.json(await Bookmark.find())
+    } catch(error){
+        res.status(400).json(error)
+    }
+})
+
+// Create
+router.post('/', async (req, res) => {
+    try {
+        res.json(await Bookmark.create(req.body))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+// Delete
+router.delete("/:id", async (req, res) => {
+    try {
+        res.json(await Bookmark.findByIdAndDelete(req.params.id))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+// Update
+router.put("/:id", async (req, res) => {
+    try {
+        res.json(await Bookmark.findByIdAndUpdate(req.params.id, req.body, {new: true}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+// Show
+router.get("/:id", async (req, res) => {
+    try {
+        res.json(await Bookmark.findById(req.params.id))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080
+const bookmarksController = require('./controllers/bookmarksController');
+const morgan = require('morgan');
+const cors = require('cors');
 
-app.listen(5000, () => console.log("Running on", 5000))
+app.use(cors())
+app.use(morgan("tiny"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("go to /bookmarks")
+})
+
+
+app.use('/bookmarks', bookmarksController);
+
+app.listen(PORT, () => console.log("Connected to port", PORT));
